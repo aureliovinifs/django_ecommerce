a7b32b53698bde736145175beda344239f8b21c8
Aurelio <aureliovinifs@gmail.com>
2021-01-04 18:24:36 -0300

    update urls.py

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -1,4 +1,7 @@
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path
 
@@ -8,7 +11,11 @@ urlpatterns = [
 	path('', home_page),
 	path('about/', about_page),
 	path('contact/', contact_page),
-    path('login/', login_page),
-    path('register/', register_page),
+        path('login/', login_page),
+        path('register/', register_page),
 	path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
