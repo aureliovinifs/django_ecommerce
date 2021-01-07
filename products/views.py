178b1729c67c1006fea8651f5f78c5e6c9a29608
Aurelio <aureliovinifs@gmail.com>
2021-01-07 19:07:16 -0300

    update vews.py and models.py

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
@@ -9,11 +10,12 @@ class ProductListView(ListView):
     #traz todos os produtos do banco de dados sem filtrar nada 
     queryset = Product.objects.all()
     template_name = "products/list.html"
-    
-    #def get_context_data(self):
-        #context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
-        #print(context)
-        #return context
+
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
+    #     print(context)
+    #     return context
+
 
 #Function Based View
 def product_list_view(request):
@@ -25,23 +27,29 @@ def product_list_view(request):
 
 #Class Based View
 class ProductDetailView(DetailView):
-    #traz todos os produtos do banco de dados sem filtrar nada 
-    queryset = Product.objects.all()
+    #queryset = Product.objects.all()
     template_name = "products/detail.html"
-    
 
-def get_context_data(self): 
-    context = super(ProductDetailView, self).get_context_data(*args, **kwargs) 
-    print(context) 
-    return context    
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+    
+    def get_object(self, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Esse produto não existe!")
+        return instance
 
 #Function Based View
 def product_detail_view(request, pk = None, *args, **kwargs):
-    #instance = Product.objects.get(pk = pk) #get the object id
-    instance = get_object_or_404(Product, pk = pk)
+    instance = Product.objects.get_by_id(pk)
+    print(instance)
+    if instance is None:
+        raise Http404("Esse produto não existe!")
+
     context = {
         'object': instance
     }
     return render(request, "products/detail.html", context)
-
-    
