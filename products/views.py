5f7e3a4f6073942a0232d0c39025a7782d334104
Aurelio <aureliovinifs@gmail.com>
2021-01-07 20:42:34 -0300

    cirando tratamento de retorno de multiplos objetos in pdoducts detail

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+
 from django.http import Http404
 
 from django.views.generic import ListView, DetailView
@@ -9,15 +10,12 @@ class ProductFeaturedListView(ListView):
     template_name = "products/list.html"
     
     def get_queryset(self, *args, **kwargs):
+        request = self.request
         return Product.objects.featured()
 
 class ProductFeaturedDetailView(DetailView):
     queryset = Product.objects.all().featured()
     template_name = "products/featured-detail.html"
-
-    #def get_queryset(self, *args, **kwargs):
-        #request = self.request
-        #return Product.objects.featured()
 
 #Class Based View
 class ProductListView(ListView):
@@ -39,6 +37,22 @@ def product_list_view(request):
     }
     return render(request, "products/list.html", context)
 
+class ProductDetailSlugView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    def get_object(self, *args, **kwargs):
+        slug = self.kwargs.get('slug')
+        #instance = get_object_or_404(Product, slug = slug, active = True)
+        try:
+            instance = Product.objects.get(slug = slug, active = True)
+        except Product.DoesNotExist:
+            raise Http404("Não encontrado!")
+        except Product.MultipleObjectsReturned:
+            qs = Product.objects.filter(slug = slug, active = True)
+            instance =  qs.first()
+        return instance
+
 #Class Based View
 class ProductDetailView(DetailView):
     template_name = "products/detail.html"
@@ -54,6 +68,7 @@ class ProductDetailView(DetailView):
         if instance is None:
             raise Http404("Esse produto não existe!")
         return instance
+
 
 #Function Based View
 def product_detail_view(request, pk = None, *args, **kwargs):
