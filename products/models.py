4bbb99f49bcb588d68d2e5edbd28bbc1530c8854
Aurelio <aureliovinifs@gmail.com>
2021-01-18 18:01:59 -0300

    reverse url implements in the project

--- products/models.py
+++ products/models.py
@@ -3,6 +3,7 @@ import random
 import os
 from django.db import models
 from django.db.models.signals import pre_save, post_save
+from django.urls import reverse
 
 from .utils import unique_slug_generator
 
@@ -61,7 +62,9 @@ class Product(models.Model): #product_category
     objects     = ProductManager()
 
     def get_absolute_url(self):
-        return "/products/{slug}/".format(slug = self.slug)
+        #return "/products/{slug}/".format(slug = self.slug)
+        return reverse("products:detail", kwargs={"slug": self.slug})
+
 
     def __str__(self):
         return self.title
--- products/urls.py
+++ products/urls.py
@@ -1,4 +1,7 @@
+
 from django.urls import path
+
+app_name = "products"
 
 from .views import (
                         ProductListView, 
@@ -7,5 +10,5 @@ from .views import (
 
 urlpatterns = [
     path('', ProductListView.as_view()),
-    path('<slug:slug>/', ProductDetailSlugView.as_view())
+    path('<slug:slug>/', ProductDetailSlugView.as_view(), name='detail')
 ]
