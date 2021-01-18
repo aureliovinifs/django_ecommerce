7f80fb69b68fe9f2fa3f0bad8949ebeaed6c1185
Aurelio <aureliovinifs@gmail.com>
2021-01-18 18:58:39 -0300

    muitas mudanças que me irritaram

--- products/urls.py
+++ products/urls.py
@@ -1,14 +1,12 @@
-
 from django.urls import path
 
 app_name = "products"
 
 from .views import (
-                        ProductListView, 
-                        ProductDetailSlugView,
-                    )
+                        ProductListView,
+                        ProductDetailSlugView,)
 
 urlpatterns = [
-    path('', ProductListView.as_view()),
+    path('', ProductListView.as_view(), name='list'),
     path('<slug:slug>/', ProductDetailSlugView.as_view(), name='detail')
 ]
